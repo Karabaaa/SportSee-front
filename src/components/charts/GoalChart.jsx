56c6642ef8d6d9f48f9850dbf7f2567a1c0df122
Amandine M <amandine.moigno@gmail.com>
2026-06-26 13:10:44 +0200

    fix: warning for responsiveContainer in GoalChart

--- src/components/charts/GoalChart.jsx
+++ src/components/charts/GoalChart.jsx
@@ -1,10 +1,5 @@
 import styles from "./GoalChart.module.css";
-import {
-  RadialBarChart,
-  RadialBar,
-  PolarAngleAxis,
-  ResponsiveContainer,
-} from "recharts";
+import { RadialBarChart, RadialBar, PolarAngleAxis } from "recharts";
 import PropTypes from "prop-types";
 
 export const GoalChart = ({ data }) => {
@@ -21,31 +16,31 @@ export const GoalChart = ({ data }) => {
           objectif
         </p>
       </div>
-      <ResponsiveContainer width="100%" aspect={1}>
-        <RadialBarChart
-          cx="50%"
-          cy="50%"
-          innerRadius="58%"
-          outerRadius="80%"
-          barSize={10}
-          data={chartData}
-          startAngle={90}
-          endAngle={450}
-        >
-          <PolarAngleAxis
-            type="number"
-            domain={[0, 100]}
-            tick={false}
-            tickLine={false}
-            axisLine={false}
-          />
-          <RadialBar
-            dataKey="score"
-            fill="var(--primary-color)"
-            cornerRadius={50}
-          />
-        </RadialBarChart>
-      </ResponsiveContainer>
+      <RadialBarChart
+        style={{ width: "100%", height: "100%" }}
+        responsive
+        cx="50%"
+        cy="50%"
+        innerRadius="58%"
+        outerRadius="80%"
+        barSize={10}
+        data={chartData}
+        startAngle={90}
+        endAngle={450}
+      >
+        <PolarAngleAxis
+          type="number"
+          domain={[0, 100]}
+          tick={false}
+          tickLine={false}
+          axisLine={false}
+        />
+        <RadialBar
+          dataKey="score"
+          fill="var(--primary-color)"
+          cornerRadius={50}
+        />
+      </RadialBarChart>
     </div>
   );
 };
